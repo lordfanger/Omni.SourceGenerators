85dafa323aed41603e665c4b164d960e0af03273
Ondřej Kubíček <lordfanger@gmail.com>
2024-11-28 22:31:14 +0100

    Add project files.

---
+++ src/Omni.SourceGenerators.Core/GeneratedTypeKind.cs
@@ -0,0 +1,25 @@
+﻿namespace Omni.SourceGenerators.Core;
+
+/// <summary>
+/// Kind of generated type.
+/// </summary>
+public enum GeneratedTypeKind
+{
+    /// <summary>
+    /// Reference object.
+    /// <code>class</code>
+    /// </summary>
+    Class,
+
+    /// <summary>
+    /// Interface.
+    /// <code>interface</code>
+    /// </summary>
+    Interface,
+
+    /// <summary>
+    /// Value object.
+    /// <code>struct</code>
+    /// </summary>
+    Struct
+}
